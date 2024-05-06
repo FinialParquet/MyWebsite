3aa8ba1ff186d964281fe18a27b88861347bf3c9
FinialParquet <169063804+FinialParquet@users.noreply.github.com>
2024-05-06 14:49:56 -0400

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-function register() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    // Basic example using btoa() for encoding (not secure for real applications)
-    const encoded = btoa(username + ':' + password);
-    localStorage.setItem('credentials', encoded);
-    alert('Registration Complete. Your data is saved locally.');
-}
